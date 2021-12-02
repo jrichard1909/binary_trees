62ed2a2439eda219a16841112c6fd45e4e66387c
unknown <jrichard1909@gmail.com>
2021-12-01 22:46:26 -0500

    1-binary_tree_insert_left.c updated

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,11 +16,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	left_node = malloc(sizeof(binary_tree_t));
+	left_node = binary_tree_node(parent, value);
 	if (left_node == NULL)
 		return (NULL);
-
-	left_node = binary_tree_node(parent, value);
 
 	if (parent->left != NULL)
 	{
